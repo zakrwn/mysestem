f047a9759263e6ebe2ecd370fe5e6e2472a667e5
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-07 22:12:05 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -392,7 +392,7 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
-});
+
 
 
 client.on("message", message => {
