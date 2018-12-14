07dff9def71e87163bff8006348f8ee63445ed3d
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-14 18:52:01 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1065,4 +1065,15 @@ client.on('message', message => {
 
 
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`.help | Golden Server`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
+
 client.login(process.env.BOT_TOKEN);
