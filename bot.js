f66dfb9bf2b06195a5e0295f859e3153929e56ef
zakrwn <45009413+zakrwn@users.noreply.github.com>
2019-01-02 17:18:11 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1067,33 +1067,7 @@ client.on("message", message => {
 
 
 
-
-
-client.on('message', message => {
-     var prefix = ".";
-  let command = message.content.split(" ")[0].slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
-
-  if(!message.content.toLowerCase().startsWith(prefix)) return;
-  if(command == "sugg") {
-    if(!args.join(" ")) return message.channel.send(`**يرجي كتابة الاقتراح **`);
-    let channel = message.guild.channels.find(c => c.name == "suggestions");
-    let embed = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.displayAvatarURL)
-    .setTitle(``)
-    .setFooter(`Select a reaction below to vote on suggestion`)
-    .setDescription(args.join(" "));
-    channel.send(embed).then(msg => {
-      msg.react("✅").then(() => msg.react("❌"));
-      message.delete()
-      message.channel.send(`**يرجي كتابة اقتراح لكي يتم ارساله الي روم الاقتراحات ❎ **`);
-    });
-
-});
-
-
-
-
+\
 
 
 
